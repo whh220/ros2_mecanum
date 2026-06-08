242aebae9eb2726d9fd72c83f6147f088e38e04d
whh220 <2560717366@qq.com>
2026-06-08 21:46:02 +0800

    odom

---
+++ src/sam_bot_odom_py/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'sam_bot_odom_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+        ('share/' + package_name + '/config', glob('config/*.yaml')),
+        ('share/' + package_name + '/config', glob('config/*.rviz')),
+        ('share/' + package_name + '/urdf', glob('urdf/*.urdf')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='wh',
+    maintainer_email='wh@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'OdometryNode = sam_bot_odom_py.OdometryNode:main'
+        ],
+    },
+)
