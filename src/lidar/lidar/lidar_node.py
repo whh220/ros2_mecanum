5ea5600f5ccabeef3c11a76c0da8d29399045f9a
whh220 <2560717366@qq.com>
2026-06-16 16:00:29 +0800

    1

--- src/lidar/lidar/lidar_node.py
+++ src/lidar/lidar/lidar_node.py
@@ -199,7 +199,7 @@ class Delta2ANode(Node):
                 self._scan[angle_deg] = dist_m
                 points_added += 1
             else:
-                self._scan[angle_deg] = RANGE_MAX + 1.0  # 无效点保存一个特殊值
+                self._scan[angle_deg] = float("inf")
         
         if self._frame_count % 100 == 0:
             self.get_logger().debug(
