78323c9a21055cc87c186a9bde7bd0bd2db47275
whh220 <2560717366@qq.com>
2026-06-19 18:49:36 +0800

    lidar

--- src/sam_bot_odom_py/launch/sam_bot_odom.launch.py
+++ src/sam_bot_odom_py/launch/sam_bot_odom.launch.py
@@ -66,7 +66,7 @@ def generate_launch_description():
         name='delta2a_lidar',
         output='screen',
         parameters=[{
-            'port':     '/dev/ttyUSB0',
+            'port':     '/dev/ttyUSB1',
             'baud':     230400,
             'frame_id': 'lidar_link',
             'topic':    'scan',
